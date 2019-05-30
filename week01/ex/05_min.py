a970461b970f391beec75446e77415cee8cac963
The_wrath <45766633+Sovicheanhim@users.noreply.github.com>
2019-05-30 21:15:43 +0700

    added exercises_05

---
+++ week01/ex/05_min.py
@@ -0,0 +1,16 @@
+num1 = input("Enter a number :")
+num1 = int(num1)
+num2 = input("Enter a number :")
+num2 = int(num2)
+operator = "<"
+if num1 > num2:
+    biggerNum = num1
+    smallerNum = num2
+elif num1 < num2:
+    biggerNum = num2
+    smallerNum = num1
+else:
+    operator = "=="
+    biggerNum = num1
+    smallerNum = num2
+print("Result : "+str(smallerNum)+" "+operator+" "+str(biggerNum))
